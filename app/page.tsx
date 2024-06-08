2e1eca836c5697207ec6d1122bcb0de3eccb24e6
Majtthieu <matthieu.jankowiak@yahoo.fr>
2024-06-08 11:17:19 +0200

    CTA pour fin de page principale et footer avec credits

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import CallToAction from "@/components/CallToAction";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <CallToAction />
     </main>
   );
 }
